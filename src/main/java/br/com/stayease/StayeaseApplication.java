d892d475ea9f3a939eb42548cce5ebdfba6772d0
Daniel Santos <daniel.victor@tqi.com.br>
2023-12-03 10:41:33 -0300

    Created Swagger documentation to document API endpoints and configurations. Also added a README file to provide project information, setup instructions, and contribution guidelines.

--- src/main/java/br/com/stayease/StayeaseApplication.java
+++ src/main/java/br/com/stayease/StayeaseApplication.java
@@ -1,9 +1,14 @@
 package br.com.stayease;
 
+import br.com.stayease.config.StayEasyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = "br.com.stayease")
+@EnableConfigurationProperties({StayEasyProperties.class})
 public class StayeaseApplication {
 
 	public static void main(String[] args) {
